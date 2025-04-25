ad3549c9983833d4c76342e699c225bd593a1d68
finify <ifeanyiizuegbu@yahoo.com>
2025-04-25 16:03:33 +0100

    Add Clarity tracking script to layout

--- resources/views/home/layout/layout.blade.php
+++ resources/views/home/layout/layout.blade.php
@@ -23,6 +23,14 @@
 	<link rel="stylesheet" href="/assets/css/custom.css" />
 	<!-- Development css -->
 	<link rel="stylesheet" href="/assets/css/style.css" />
+
+	<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r9q12g8mwi");
+</script>
 	
 	<!-- Production css -->
 	<!-- <link rel="stylesheet" href="//assets/css/style.min.css" /> -->
